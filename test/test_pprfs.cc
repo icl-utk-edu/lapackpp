37c283991786b23668f6fadd42f5827c2a94d22c
Jamie Finney <jmfinney@icl.utk.edu>
2017-12-12 15:22:26 -0500

    Added Cholesky packed tests.  TODO add Anrom calculation to ppcon test.

---
+++ test/test_pprfs.cc
@@ -0,0 +1,175 @@
+#include "test.hh"
+#include "lapack.hh"
+#include "lapack_flops.hh"
+#include "print_matrix.hh"
+#include "error.hh"
+
+#include <vector>
+#include <omp.h>
+
+// -----------------------------------------------------------------------------
+// simple overloaded wrappers around LAPACKE
+static lapack_int LAPACKE_pprfs(
+    char uplo, lapack_int n, lapack_int nrhs, float* AP, float* AFP, float* B, lapack_int ldb, float* X, lapack_int ldx, float* ferr, float* berr )
+{
+    return LAPACKE_spprfs( LAPACK_COL_MAJOR, uplo, n, nrhs, AP, AFP, B, ldb, X, ldx, ferr, berr );
+}
+
+static lapack_int LAPACKE_pprfs(
+    char uplo, lapack_int n, lapack_int nrhs, double* AP, double* AFP, double* B, lapack_int ldb, double* X, lapack_int ldx, double* ferr, double* berr )
+{
+    return LAPACKE_dpprfs( LAPACK_COL_MAJOR, uplo, n, nrhs, AP, AFP, B, ldb, X, ldx, ferr, berr );
+}
+
+static lapack_int LAPACKE_pprfs(
+    char uplo, lapack_int n, lapack_int nrhs, std::complex<float>* AP, std::complex<float>* AFP, std::complex<float>* B, lapack_int ldb, std::complex<float>* X, lapack_int ldx, float* ferr, float* berr )
+{
+    return LAPACKE_cpprfs( LAPACK_COL_MAJOR, uplo, n, nrhs, AP, AFP, B, ldb, X, ldx, ferr, berr );
+}
+
+static lapack_int LAPACKE_pprfs(
+    char uplo, lapack_int n, lapack_int nrhs, std::complex<double>* AP, std::complex<double>* AFP, std::complex<double>* B, lapack_int ldb, std::complex<double>* X, lapack_int ldx, double* ferr, double* berr )
+{
+    return LAPACKE_zpprfs( LAPACK_COL_MAJOR, uplo, n, nrhs, AP, AFP, B, ldb, X, ldx, ferr, berr );
+}
+
+// -----------------------------------------------------------------------------
+template< typename scalar_t >
+void test_pprfs_work( Params& params, bool run )
+{
+    using namespace blas;
+    typedef typename traits< scalar_t >::real_t real_t;
+    typedef long long lld;
+
+    // get & mark input values
+    lapack::Uplo uplo = params.uplo.value();
+    int64_t n = params.dim.n();
+    int64_t nrhs = params.nrhs.value();
+    int64_t align = params.align.value();
+
+    // mark non-standard output values
+    params.ref_time.value();
+    //params.ref_gflops.value();
+    //params.gflops.value();
+
+    if (! run)
+        return;
+
+    // ---------- setup
+    int64_t ldb = roundup( max( 1, n ), align );
+    int64_t ldx = roundup( max( 1, n ), align );
+    size_t size_AP = (size_t) (n*(n+1)/2);
+    size_t size_AFP = (size_t) (n*(n+1)/2);
+    size_t size_B = (size_t) ldb * nrhs;
+    size_t size_X = (size_t) ldx * nrhs;
+    size_t size_ferr = (size_t) (nrhs);
+    size_t size_berr = (size_t) (nrhs);
+
+    std::vector< scalar_t > AP( size_AP );
+    std::vector< scalar_t > AFP( size_AFP );
+    std::vector< scalar_t > B( size_B );
+    std::vector< scalar_t > X_tst( size_X );
+    std::vector< scalar_t > X_ref( size_X );
+    std::vector< real_t > ferr_tst( size_ferr );
+    std::vector< real_t > ferr_ref( size_ferr );
+    std::vector< real_t > berr_tst( size_berr );
+    std::vector< real_t > berr_ref( size_berr );
+
+    int64_t idist = 1;
+    int64_t iseed[4] = { 0, 1, 2, 3 };
+    lapack::larnv( idist, iseed, AP.size(), &AP[0] );
+    lapack::larnv( idist, iseed, AFP.size(), &AFP[0] );
+    lapack::larnv( idist, iseed, B.size(), &B[0] );
+    lapack::larnv( idist, iseed, X_tst.size(), &X_tst[0] );
+    X_ref = X_tst;
+    
+    // diagonally dominant -> positive definite
+    if (uplo == lapack::Uplo::Upper) {
+        for (int64_t i = 0; i < n; ++i) {
+            AP[ i + 0.5*(i+1)*i ] += n;
+        }
+    }
+    else { // lower
+        for (int64_t i = 0; i < n; ++i) {
+            AP[ i + n*i - 0.5*i*(i+1) ] += n;
+        }
+    }
+    
+    AFP = AP;
+    
+    // factor AFP
+    int64_t info = lapack::pptrf( uplo, n, &AFP[0] );
+    if (info != 0) {
+        fprintf( stderr, "lapack::pptrf returned error %lld\n", (lld) info );
+    }
+    
+    // compute solution
+    info = lapack::pptrs( uplo, n, nrhs, &AFP[0], &B[0], ldb );
+    if (info != 0) {
+        fprintf( stderr, "lapack::pptrs returned error %lld\n", (lld) info );
+    }
+
+    // ---------- run test
+    libtest::flush_cache( params.cache.value() );
+    double time = omp_get_wtime();
+    int64_t info_tst = lapack::pprfs( uplo, n, nrhs, &AP[0], &AFP[0], &B[0], ldb, &X_tst[0], ldx, &ferr_tst[0], &berr_tst[0] );
+    time = omp_get_wtime() - time;
+    if (info_tst != 0) {
+        fprintf( stderr, "lapack::pprfs returned error %lld\n", (lld) info_tst );
+    }
+
+    params.time.value() = time;
+    //double gflop = lapack::Gflop< scalar_t >::pprfs( n, nrhs );
+    //params.gflops.value() = gflop / time;
+
+    if (params.ref.value() == 'y' || params.check.value() == 'y') {
+        // ---------- run reference
+        libtest::flush_cache( params.cache.value() );
+        time = omp_get_wtime();
+        int64_t info_ref = LAPACKE_pprfs( uplo2char(uplo), n, nrhs, &AP[0], &AFP[0], &B[0], ldb, &X_ref[0], ldx, &ferr_ref[0], &berr_ref[0] );
+        time = omp_get_wtime() - time;
+        if (info_ref != 0) {
+            fprintf( stderr, "LAPACKE_pprfs returned error %lld\n", (lld) info_ref );
+        }
+
+        params.ref_time.value() = time;
+        //params.ref_gflops.value() = gflop / time;
+
+        // ---------- check error compared to reference
+        real_t error = 0;
+        if (info_tst != info_ref) {
+            error = 1;
+        }
+        error += abs_error( X_tst, X_ref );
+        error += abs_error( ferr_tst, ferr_ref );
+        error += abs_error( berr_tst, berr_ref );
+        params.error.value() = error;
+        params.okay.value() = (error == 0);  // expect lapackpp == lapacke
+    }
+}
+
+// -----------------------------------------------------------------------------
+void test_pprfs( Params& params, bool run )
+{
+    switch (params.datatype.value()) {
+        case libtest::DataType::Integer:
+            throw std::exception();
+            break;
+
+        case libtest::DataType::Single:
+            test_pprfs_work< float >( params, run );
+            break;
+
+        case libtest::DataType::Double:
+            test_pprfs_work< double >( params, run );
+            break;
+
+        case libtest::DataType::SingleComplex:
+            test_pprfs_work< std::complex<float> >( params, run );
+            break;
+
+        case libtest::DataType::DoubleComplex:
+            test_pprfs_work< std::complex<double> >( params, run );
+            break;
+    }
+}

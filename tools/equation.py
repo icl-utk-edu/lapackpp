fd28801bce8ea15a5cdcc36ac6986fc2bee20112
Mark Gates <mgates3@icl.utk.edu>
2018-04-05 14:15:34 -0400

    Add doxygen filter for Latex-style $...$ and \[...\] equations.

--- tools/equation.py
+++ tools/equation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #
-# Attempts to find equations in Doxygen code and replace them with \f$ ... \f$.
+# Attempts to find equations in Doxygen code and replace them with $...$.
 # For instance:
 #     /// solve A * X = B^H for matrices A, X, B.
 # becomes
-#     /// solve \f$ A X = B^H \f$ for matrices A, X, B.
+#     /// solve $A X = B^H$ for matrices A, X, B.
 # However, patterns are heuristic and incomplete, so mistakes occur.
 # Use with caution.
 
@@ -15,89 +15,110 @@ import re
 
 debug = False
 
+operators = {
+    '<=': r'\le',
+    '>=': r'\ge',
+}
+
 def equation( line, subexpr=False ):
     if (debug):
-        print( 'equation:', line, 'subexpr', subexpr )
+        print( 'equation:', line + '; subexpr:', subexpr )
 
     if (subexpr):
         eqn = ''
     else:
-        eqn = r'\f$ '
-    space = True
+        eqn = r'$'
+    begin = True
     while (line):
+        # no space after $ at beginning;
+        # after that, add a space before most tokens (except ^T and punctuation)
+        if (begin):
+            begin = False
+            space = ''
+        else:
+            space = ' '
+
         if (debug):
             print( 'eqn: <' + eqn + '> line: <' + line + '>' )
 
         # space
         s = re.search( r'^( +)(.*)', line )
         if (s):
-            if (not space):
-                eqn += s.group(1)
             line = s.group(2)
-            space = True
             continue
 
-        # variable or number
-        s = re.search( r'^([A-Z]|VT|\d+)\b(.*)', line )
+        # variable
+        s = re.search( r'^([a-zA-Z]|VT)(\d*)\b(.*)', line )
         if (s):
-            eqn += s.group(1)
+            eqn += space + s.group(1)
+            if (s.group(2)):
+                eqn += '_{' + s.group(2) + '}'
+            line = s.group(3)
+            continue
+
+        # number
+        s = re.search( r'^(\d+)\b(.*)', line )
+        if (s):
+            eqn += space + s.group(1)
             line = s.group(2)
-            space = False
             continue
 
         # binary operator * becomes implicit
         s = re.search( r'^(\*)(.*)', line )
         if (s):
-            if (not space):
-                eqn += ' '
             line = s.group(2)
-            space = True
             continue
 
-        # explicit binary operators + - /
-        s = re.search( r'^([=\+\-\/])(.*)', line )
+        # explicit binary operators <= >=
+        s = re.search( r'^(<=|>=)(.*)', line )
         if (s):
-            eqn += s.group(1)
+            eqn += space + operators[ s.group(1) ]
             line = s.group(2)
-            space = False
+            continue
+
+        # explicit binary operators = < > + - /
+        s = re.search( r'^([=<>\+\-\/])(.*)', line )
+        if (s):
+            eqn += space + s.group(1)
+            line = s.group(2)
             continue
 
         # exponents, transpose
         s = re.search( r'^(\^[HT2-9])\b(.*)', line )
         if (s):
+            # don't print space
             eqn += s.group(1)
             line = s.group(2)
-            space = False
             continue
 
         # Greek, functions
-        s = re.search( r'^(alpha|beta|lambda|Lambda|sigma|Sigma|tau|sqrt|min|max)\b(.*)', line )
+        s = re.search( r'^(alpha|beta|lambda|Lambda|mu|sigma|Sigma|tau|sqrt|min|max)\b(.*)', line )
         if (s):
-            eqn += '\\' + s.group(1)
+            eqn += space + '\\' + s.group(1)
             line = s.group(2)
-            space = False
             continue
 
         # capitalized Greek, functions
         s = re.search( r'^(MIN|MAX)\b(.*)', line )
         if (s):
-            eqn += '\\' + s.group(1).lower()
+            eqn += space + '\\' + s.group(1).lower()
             line = s.group(2)
-            space = False
             continue
 
         # norm
+        # todo: use \lVert?
         s = re.search( r'^norm\((.*)', line )
         if (s):
             (expr, line) = equation( s.group(1), True )
-            eqn += r'|| ' + expr + ' ||'
+            eqn += space + r'|| ' + expr + ' ||'
             continue
 
         # abs
+        # todo: use \lvert?
         s = re.search( r'^abs\((.*)', line )
         if (s):
             (expr, line) = equation( s.group(1), True )
-            eqn += r'| ' + expr + ' |'
+            eqn += space + r'| ' + expr + ' |'
             continue
 
         # inverse
@@ -106,19 +127,16 @@ def equation( line, subexpr=False ):
             (expr, line) = equation( s.group(1), True )
             if (re.search( '^\w+$', expr )):
                 # safe to skip parens
-                eqn += expr + '^{-1}'
+                eqn += space + expr + '^{-1}'
             else:
-                eqn += '(' + expr + ')^{-1}'
-            space = True
+                eqn += space + '(' + expr + ')^{-1}'
             continue
 
         # non-Latex functions
         s = re.search( r'^(diag)\((.*)', line )
         if (s):
             (expr, line) = equation( s.group(2), True )
-            if (not space): eqn += ' '
-            eqn += r'\; \text{' + s.group(1) + '}(' + expr + ') \; '
-            space = True
+            eqn += space + r'\; \text{' + s.group(1) + '}(' + expr + ') \; '
             continue
 
         # open parens
@@ -126,9 +144,9 @@ def equation( line, subexpr=False ):
         if (s):
             (expr, line) = equation( s.group(1), True )
             if (expr):
-                eqn += '(' + expr + ')'
-                space = False
+                eqn += space + '(' + expr + ')'
             else:
+                # sub-expr didn't parse, so put ( back on line
                 line = '(' + line
                 break # done
             continue
@@ -146,60 +164,65 @@ def equation( line, subexpr=False ):
         # ellipsis
         s = re.search( r'^\. ?\. ?\.(.*)', line )
         if (s):
-            if (not space):
-                eqn += ' '
-            eqn += r'\dots '
-            space = True
+            eqn += space + r'\dots'
             line = s.group(1)
             continue
 
         # punctuation
-        s = re.search( r'^([.,;:])(.*)', line )
+        ##s = re.search( r'^([.,;:])(.*)', line )
+        s = re.search( r'^([,])(.*)', line )
         if (s):
+            # don't print space
             eqn += s.group(1)
             line = s.group(2)
-            space = False
             continue
 
         # otherwise done
         break
     # end
     if (not subexpr):
-        if (space):
-            eqn += r'\f$ '
-        else:
-            eqn += r' \f$'
+        eqn += '$'
+        # add space after $,
+        # unless there's punctuation or already space such as \n in line.
+        s = re.search( r'^[^.,;:\s]', line )
+        if (s):
+            eqn += ' '
     return (eqn, line)
 # end
 
 def process( arg ):
     filename = arg + '.cc'
-    f_out = open( '../eqn/' + filename, 'w' )
-    f_in  = open( '../gen/' + filename )
+    f_out = '../eqn/' + filename
+    f_in  = '../gen/' + filename
+    print( 'reading ' + f_in + '\nwriting ' + f_out )
+    f_out = open( f_out, 'w' )
+    f_in  = open( f_in )
     for line in f_in:
         s = re.search( r'^///', line )
         if (s):
-            line = line.rstrip()
-            ##if (re.search( r'^(.*?)\b((?:[A-Z]|VT) *(?:=|\^|\+|\-|\*|\/).*)', line )):
-            ##    print( line, file=f_out )
-            line2 = ''
-            while (line):
-                s = re.search( r'^(.*?)\b((?:(?:[A-Z]|VT) *(?:=|\^|\+|\-|\*|\/)|norm\(|inv\(|diag\(|sqrt\(|alpha|beta|lambda|sigma|tau).*)', line )
+            rest = line.rstrip()
+            line = ''
+            while (rest):
+                #                                variable        Greek                                            operator          | functions
+                s = re.search( r'^(.*?)\b((?: (?:[A-Z]\d* | VT | alpha | beta | lambda | mu | sigma | tau) \s* (?:=|\^|\+|\-|\*|\/) | norm\( | inv\( | diag\( | sqrt\( ).*)',
+                               rest, re.X )
                 if (s):
-                    line2 += s.group(1)
-                    (eqn, line) = equation( s.group(2) )
-                    eqn = re.sub( r'(\\f\$)([.,;:])', r'\2\1', eqn )
-                    line2 += eqn
+                    line += s.group(1)
+                    (eqn, rest) = equation( s.group(2) )
+                    if (debug):
+                        print( 'eqn: ' + eqn + ', rest: ' + rest )
+                    eqn = re.sub( r',\$( *)$', r'$,\1', eqn )  # move trailing punctuation (only commas) outside
+                    line += eqn
                 else:
                     break
                 # end
             # end
-            print( line2 + line, file=f_out )
-            ##print( file=f_out )
-            ##end
-        else:
-            print( line, file=f_out, end='' )
+            line += rest + '\n'
+            # if the entire line is an indented equation, make it \[ ... \]
+            # and put punctuation inside.
+            line = re.sub( r'^///     \$(.*)\$([.,;:]?)$', r'///     \[ \1\2 \]', line )
         # end
+        print( line, file=f_out, end='' )
     # end
 # end
 
@@ -207,6 +230,5 @@ for arg in sys.argv[1:]:
     if (arg == '-d'):
         debug = True
     else:
-        print( arg )
         process( arg )
 # end

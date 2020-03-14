add174132790da791290262bd0164f87d881a24c
Mark Gates <mgates3@icl.utk.edu>
2020-03-14 01:09:01 -0400

    Pretty print version

--- test/test.cc
+++ test/test.cc
@@ -671,8 +671,10 @@ int main( int argc, char** argv )
 
     int status = 0;
     try {
-        printf( "LAPACK++ version %d, id %s\n",
-                lapack::lapackpp_version(), lapack::lapackpp_id() );
+        int version = lapack::lapackpp_version();
+        printf( "LAPACK++ version %d.%02d.%02d, id %s\n",
+                version / 10000, (version % 10000) / 100, version % 100,
+                lapack::lapackpp_id() );
 
         // print input so running `test [input] > out.txt` documents input
         printf( "input: %s", argv[0] );

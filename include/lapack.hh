8b90f1e7ad0e8838af79a6cdafbec98eab08a885
Mark Gates <mgates3@icl.utk.edu>
2020-10-28 09:00:55 -0400

    Version 2020.10.02

--- include/lapack.hh
+++ include/lapack.hh
@@ -7,8 +7,8 @@
 #define LAPACK_HH
 
 // Version is updated by make_release.py; DO NOT EDIT.
-// Version 2020.10.01
-#define LAPACKPP_VERSION 20201001
+// Version 2020.10.02
+#define LAPACKPP_VERSION 20201002
 
 namespace lapack {
 

fff8a6c1dc7e929f0b9b08eb2cbd78ac4f89098b
Mark Gates <mgates3@icl.utk.edu>
2021-04-27 01:38:11 -0400

    pbtrs backward error check

--- test/test_pbtrs.cc
+++ test/test_pbtrs.cc
@@ -9,6 +9,7 @@
 #include "print_matrix.hh"
 #include "error.hh"
 #include "lapacke_wrappers.hh"
+#include "cblas_wrappers.hh"
 
 #include <vector>
 
@@ -25,6 +26,10 @@ void test_pbtrs_work( Params& params, bool run )
     int64_t kd = params.kd();
     int64_t nrhs = params.nrhs();
     int64_t align = params.align();
+    int64_t verbose = params.verbose();
+
+    real_t eps = std::numeric_limits< real_t >::epsilon();
+    real_t tol = params.tol() * eps;
 
     // mark non-standard output values
     params.ref_time();
@@ -40,31 +45,45 @@ void test_pbtrs_work( Params& params, bool run )
     size_t size_AB = (size_t) ldab * n;
     size_t size_B = (size_t) ldb * nrhs;
 
-    std::vector< scalar_t > AB( size_AB );
+    std::vector< scalar_t > AB_tst( size_AB );
+    std::vector< scalar_t > AB_ref( size_AB );
     std::vector< scalar_t > B_tst( size_B );
     std::vector< scalar_t > B_ref( size_B );
 
     int64_t idist = 1;
     int64_t iseed[4] = { 0, 1, 2, 3 };
-    lapack::larnv( idist, iseed, AB.size(), &AB[0] );
+    lapack::larnv( idist, iseed, AB_tst.size(), &AB_tst[0] );
     lapack::larnv( idist, iseed, B_tst.size(), &B_tst[0] );
 
     // diagonally dominant -> positive definite
     if (uplo == lapack::Uplo::Upper) {
         for (int64_t j = 0; j < n; ++j) {
-            AB[ kd + j*ldab ] += n;
+            AB_tst[ kd + j*ldab ] += n;
         }
     }
     else { // lower
         for (int64_t j = 0; j < n; ++j) {
-            AB[ j*ldab ] += n;
+            AB_tst[ j*ldab ] += n;
         }
     }
 
+    AB_ref = AB_tst;
     B_ref = B_tst;
 
+    if (verbose >= 1) {
+        printf( "\n"
+                "AB n=%5lld, kd=%5lld, ldab=%5lld\n"
+                "B n=%5lld, nrhs=%5lld, ldb=%5lld\n",
+                (lld) n, (lld) kd, (lld) ldab,
+                (lld) n, (lld) nrhs, (lld) ldb );
+    }
+    if (verbose >= 2) {
+        printf( "AB = " ); print_matrix( kd+1, n, &AB_tst[0], ldab );
+        printf( "B = " ); print_matrix( n, nrhs, &B_tst[0], ldb );
+    }
+
     // factor AB
-    int64_t info = lapack::pbtrf( uplo, n, kd, &AB[0], ldab );
+    int64_t info = lapack::pbtrf( uplo, n, kd, &AB_tst[0], ldab );
     if (info != 0) {
         fprintf( stderr, "lapack::pbtrf returned error %lldpn", (lld) info );
     }
@@ -72,7 +91,7 @@ void test_pbtrs_work( Params& params, bool run )
     // ---------- run test
     testsweeper::flush_cache( params.cache() );
     double time = testsweeper::get_wtime();
-    int64_t info_tst = lapack::pbtrs( uplo, n, kd, nrhs, &AB[0], ldab, &B_tst[0], ldb );
+    int64_t info_tst = lapack::pbtrs( uplo, n, kd, nrhs, &AB_tst[0], ldab, &B_tst[0], ldb );
     time = testsweeper::get_wtime() - time;
     if (info_tst != 0) {
         fprintf( stderr, "lapack::pbtrs returned error %lld\n", (lld) info_tst );
@@ -82,11 +101,39 @@ void test_pbtrs_work( Params& params, bool run )
     double gflop = lapack::Gflop< scalar_t >::pbtrs( n, kd, nrhs );
     params.gflops() = gflop / time;
 
-    if (params.ref() == 'y' || params.check() == 'y') {
+    if (verbose >= 2) {
+        printf( "A_factor = " ); print_matrix( kd+1, n, &AB_tst[0], ldab );
+        printf( "X = " ); print_matrix( n, nrhs, &B_tst[0], ldb );
+    }
+
+    if (params.check() == 'y') {
+        // ---------- check error
+        // Relative backwards error = ||b - Ax|| / (n * ||A|| * ||x||).
+        // No hbmm, so loop over RHS.
+        for (int64_t j = 0; j < nrhs; ++j) {
+            // B_ref -= A * B_tst
+            cblas_hbmv( CblasColMajor, cblas_uplo_const(uplo), n, kd,
+                        -1.0, &AB_ref[0], ldab,
+                              &B_tst[ j*ldb ], 1,
+                         1.0, &B_ref[ j*ldb ], 1 );
+        }
+        if (verbose >= 2) {
+            printf( "R = " ); print_matrix( n, nrhs, &B_ref[0], ldb );
+        }
+
+        real_t error = lapack::lange( lapack::Norm::One, n, nrhs, &B_ref[0], ldb );
+        real_t Xnorm = lapack::lange( lapack::Norm::One, n, nrhs, &B_tst[0], ldb );
+        real_t Anorm = lapack::lanhb( lapack::Norm::One, uplo, n, kd, &AB_ref[0], ldab );
+        error /= (n * Anorm * Xnorm);
+        params.error() = error;
+        params.okay() = (error < tol);
+    }
+
+    if (params.ref() == 'y') {
         // ---------- run reference
         testsweeper::flush_cache( params.cache() );
         time = testsweeper::get_wtime();
-        int64_t info_ref = LAPACKE_pbtrs( uplo2char(uplo), n, kd, nrhs, &AB[0], ldab, &B_ref[0], ldb );
+        int64_t info_ref = LAPACKE_pbtrs( uplo2char(uplo), n, kd, nrhs, &AB_tst[0], ldab, &B_ref[0], ldb );
         time = testsweeper::get_wtime() - time;
         if (info_ref != 0) {
             fprintf( stderr, "LAPACKE_pbtrs returned error %lld\n", (lld) info_ref );
@@ -94,15 +141,6 @@ void test_pbtrs_work( Params& params, bool run )
 
         params.ref_time() = time;
         params.ref_gflops() = gflop / time;
-
-        // ---------- check error compared to reference
-        real_t error = 0;
-        if (info_tst != info_ref) {
-            error = 1;
-        }
-        error += abs_error( B_tst, B_ref );
-        params.error() = error;
-        params.okay() = (error == 0);  // expect lapackpp == lapacke
     }
 }
 

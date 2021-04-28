4ab769cee282c29f63944cbe73acdf708e49e56d
Mark Gates <mgates3@icl.utk.edu>
2021-04-28 13:24:37 -0400

    pbrfs backward error check

--- test/test_pbrfs.cc
+++ test/test_pbrfs.cc
@@ -9,6 +9,7 @@
 #include "print_matrix.hh"
 #include "error.hh"
 #include "lapacke_wrappers.hh"
+#include "cblas_wrappers.hh"
 
 #include <vector>
 
@@ -25,8 +26,10 @@ void test_pbrfs_work( Params& params, bool run )
     int64_t kd = params.kd();
     int64_t nrhs = params.nrhs();
     int64_t align = params.align();
+    int64_t verbose = params.verbose();
 
     real_t eps = std::numeric_limits< real_t >::epsilon();
+    real_t tol = params.tol() * eps;
 
     // mark non-standard output values
     params.ref_time();
@@ -38,13 +41,13 @@ void test_pbrfs_work( Params& params, bool run )
 
     // ---------- setup
     int64_t ldab = roundup( kd+1, align );
-    int64_t ldafb = roundup( kd+1, align );
+    int64_t ldafb = ldab;
     int64_t ldb = roundup( blas::max( 1, n ), align );
-    int64_t ldx = roundup( blas::max( 1, n ), align );
+    int64_t ldx = ldb;
     size_t size_AB = (size_t) ldab * n;
-    size_t size_AFB = (size_t) ldafb * n;
+    size_t size_AFB = size_AB;
     size_t size_B = (size_t) ldb * nrhs;
-    size_t size_X = (size_t) ldx * nrhs;
+    size_t size_X = size_B;
     size_t size_ferr = (size_t) (nrhs);
     size_t size_berr = (size_t) (nrhs);
 
@@ -61,10 +64,10 @@ void test_pbrfs_work( Params& params, bool run )
     int64_t idist = 1;
     int64_t iseed[4] = { 0, 1, 2, 3 };
     lapack::larnv( idist, iseed, AB.size(), &AB[0] );
-    lapack::larnv( idist, iseed, AFB.size(), &AFB[0] );
+    int64_t iseed_B[4];
+    std::copy( iseed, iseed+4, iseed_B );
     lapack::larnv( idist, iseed, B.size(), &B[0] );
-    lapack::larnv( idist, iseed, X_tst.size(), &X_tst[0] );
-    X_ref = X_tst;
+    X_tst = B;
 
     // diagonally dominant -> positive definite
     if (uplo == lapack::Uplo::Upper) {
@@ -80,22 +83,32 @@ void test_pbrfs_work( Params& params, bool run )
 
     AFB = AB;
 
-    // factor
-    int64_t info = lapack::pbtrf( uplo, n, kd, &AFB[0], ldab );
+    // Factor
+    int64_t info = lapack::pbtrf( uplo, n, kd, &AFB[0], ldafb );
     if (info != 0) {
         fprintf( stderr, "lapack::pbtrf returned error %lld\n", (lld) info );
     }
 
-    // compute solution
-    info = lapack::pbtrs ( uplo, n, kd, nrhs, &AFB[0], ldab, &B[0], ldb );
+    // Solve in X_tst
+    info = lapack::pbtrs ( uplo, n, kd, nrhs, &AFB[0], ldab, &X_tst[0], ldx );
     if (info != 0) {
         fprintf( stderr, "lapack::pbtrs returned error %lld\n", (lld) info );
     }
+    X_ref = X_tst;
+
+    if (verbose >= 2) {
+        printf( "A_factor = " ); print_matrix( kd+1, n, &AFB[0], ldafb );
+        printf( "X = " ); print_matrix( n, nrhs, &X_tst[0], ldx );
+    }
+
 
     // ---------- run test
     testsweeper::flush_cache( params.cache() );
     double time = testsweeper::get_wtime();
-    int64_t info_tst = lapack::pbrfs( uplo, n, kd, nrhs, &AB[0], ldab, &AFB[0], ldafb, &B[0], ldb, &X_tst[0], ldx, &ferr_tst[0], &berr_tst[0] );
+    // Refine solution in X_tst, using original AB and B, factored AFB.
+    int64_t info_tst = lapack::pbrfs(
+        uplo, n, kd, nrhs, &AB[0], ldab, &AFB[0], ldafb,
+        &B[0], ldb, &X_tst[0], ldx, &ferr_tst[0], &berr_tst[0] );
     time = testsweeper::get_wtime() - time;
     if (info_tst != 0) {
         fprintf( stderr, "lapack::pbrfs returned error %lld\n", (lld) info_tst );
@@ -105,11 +118,45 @@ void test_pbrfs_work( Params& params, bool run )
     //double gflop = lapack::Gflop< scalar_t >::pbrfs( n, kd, nrhs );
     //params.gflops() = gflop / time;
 
-    if (params.ref() == 'y' || params.check() == 'y') {
+    if (verbose >= 2) {
+        printf( "Xrfs = " ); print_matrix( n, nrhs, &X_tst[0], ldx );
+        printf( "ferr = " ); print_vector( n, &ferr_tst[0], 1 );
+        printf( "berr = " ); print_vector( n, &berr_tst[0], 1 );
+    }
+
+    if (params.check() == 'y') {
+        // ---------- check error
+        // Relative backwards error = ||b - Ax|| / (n * ||A|| * ||x||).
+        // No hbmm, so loop over RHS.
+        for (int64_t j = 0; j < nrhs; ++j) {
+            // B_ref -= A * B_tst
+            cblas_hbmv( CblasColMajor, cblas_uplo_const(uplo), n, kd,
+                        -1.0, &AB[0], ldab,
+                              &X_tst[ j*ldx ], 1,
+                         1.0, &B[ j*ldb ], 1 );
+        }
+        if (verbose >= 2) {
+            printf( "R = " ); print_matrix( n, nrhs, &B[0], ldb );
+        }
+
+        real_t error = lapack::lange( lapack::Norm::One, n, nrhs, &B[0], ldb );
+        real_t Xnorm = lapack::lange( lapack::Norm::One, n, nrhs, &X_tst[0], ldx );
+        real_t Anorm = lapack::lanhb( lapack::Norm::One, uplo, n, kd, &AB[0], ldab );
+        error /= (n * Anorm * Xnorm);
+        params.error() = error;
+        params.okay() = (error < tol);
+
+        // Reset B for ref using saved seed.
+        lapack::larnv( idist, iseed_B, B.size(), &B[0] );
+    }
+
+    if (params.ref() == 'y') {
         // ---------- run reference
         testsweeper::flush_cache( params.cache() );
         time = testsweeper::get_wtime();
-        int64_t info_ref = LAPACKE_pbrfs( uplo2char(uplo), n, kd, nrhs, &AB[0], ldab, &AFB[0], ldafb, &B[0], ldb, &X_ref[0], ldx, &ferr_ref[0], &berr_ref[0] );
+        int64_t info_ref = LAPACKE_pbrfs(
+            uplo2char(uplo), n, kd, nrhs, &AB[0], ldab, &AFB[0], ldafb,
+            &B[0], ldb, &X_ref[0], ldx, &ferr_ref[0], &berr_ref[0] );
         time = testsweeper::get_wtime() - time;
         if (info_ref != 0) {
             fprintf( stderr, "LAPACKE_pbrfs returned error %lld\n", (lld) info_ref );
@@ -117,17 +164,6 @@ void test_pbrfs_work( Params& params, bool run )
 
         params.ref_time() = time;
         //params.ref_gflops() = gflop / time;
-
-        // ---------- check error compared to reference
-        real_t error = 0;
-        if (info_tst != info_ref) {
-            error = 1;
-        }
-        error += abs_error( X_tst, X_ref );
-        error += abs_error( ferr_tst, ferr_ref );
-        error += abs_error( berr_tst, berr_ref );
-        params.error() = error;
-        params.okay() = (error < 3*eps);
     }
 }
 

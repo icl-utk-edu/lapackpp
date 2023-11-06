0b45dc2a63779fa7adc43aa24d610caff719f1ce
Mark Gates <mgates3@icl.utk.edu>
2023-11-06 12:33:46 -0500

    copyright 2023

--- tools/wrapper_gen.py
+++ tools/wrapper_gen.py
@@ -151,7 +151,7 @@ header_bottom = '''\
 # --------------------
 # for src/*.cc wrappers
 wrapper_top1 = '''\
-// Copyright (c) 2017-2022, University of Tennessee. All rights reserved.
+// Copyright (c) 2017-2023, University of Tennessee. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
@@ -179,7 +179,7 @@ wrapper_bottom = '''\
 # --------------------
 # for test/test_*.cc testers
 tester_top = '''\
-// Copyright (c) 2017-2022, University of Tennessee. All rights reserved.
+// Copyright (c) 2017-2023, University of Tennessee. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
